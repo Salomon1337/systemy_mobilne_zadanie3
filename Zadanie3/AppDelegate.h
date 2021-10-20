9fa50a019efb3117abc3fe50a17f8ff8ed3934c2
Mariusz Rostkowski <salomonpl2000@gmail.com>
2021-10-20 14:17:52 +0200

    Add files via upload

---
+++ Zadanie3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Zadanie3
+//
+//  Created by student on 19/10/2021.
+//  Copyright © 2021 PB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Zadanie3/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Zadanie3
+//
+//  Created by student on 19/10/2021.
+//  Copyright © 2021 PB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIButton *informationButton;
+@property (nonatomic, weak ) IBOutlet UIImageView *image;
+@end
+
